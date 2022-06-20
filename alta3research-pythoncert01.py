5f41815decdc28c1fd5b3e38b26600528802188b
Joselin Campoverde <37119091+joselinc@users.noreply.github.com>
2022-06-20 15:32:13 -0400

    Update alta3research-pythoncert01.py

--- alta3research-pythoncert01.py
+++ alta3research-pythoncert01.py
@@ -6,12 +6,12 @@
 #import crayons package. Statement always goes on top before main code
 import crayons
 
-#print welcome to my python game title in bold red. Let the user know they have 10 tries to guess the number
+#print a title "welcome to my python game" in bold red. Let the user know they have 10 tries to guess the number
 print(crayons.red(f"Hello, Welcome to my Python game.\nCan you guess the number I'm thinking?. You have 10 tries", bold=True))
 
 """run time code. Always indent under function"""
 def status():
-    #set count to 10
+    #set a counter to 10 this is the number of tries the user will have
     count = 10
 
     while count > 0:
@@ -23,11 +23,12 @@ def status():
             print(crayons.green(f"YOU WIN", bold=True))
             print(crayons.yellow(f"The game has ended. Goodbye!", bold=True))
             break
-        #if incorrect number, display 'Wrong answer...' message. The count will decrease by 1
+        #if incorrect number, display a 'Wrong answer...' message and the number of remaining attempts
         print(crayons.blue(f"Wrong answer. You have {count-1} attempts remaining....", bold=True))
+        #counter will decrease by one 
         count = count - 1 
     else:
-        #when counter reaches 0, user lost game
+        #when counter reaches 0, user looses the game
         print(crayons.yellow(f"Sorry you lost the game :(", bold=True))
 
 def main():
